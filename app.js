544827c5fdd360a05c3aa15364a19a257bac9c3d
ohseungmin <ohmin2886@gmail.com>
2023-10-04 12:11:18 +0900

    첫 서버 만듬

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
-console.log('hello');
+const http = require('http');
+
+http.createServer(function(request, response) {
+  let writeHeadObject = {
+    'Content-Type': 'text/html'
+  }
+  
+  response.writeHead(200, writeHeadObject);
+  response.end("hello");
+
+
+}).listen(8080);
+
+// 터미널에 node app.js 입력
+// 서버 가동중이기 때문에 밑에 실행줄이 안됨
+// 서버 끄는 방법은 ctrl c 입력
+  
