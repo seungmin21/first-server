92b51d0d77537265f1ca076ab6a78329b8647194
ohseungmin <ohmin2886@gmail.com>
2023-10-04 17:42:39 +0900

    function으로 서버 작성

--- testSever.js
+++ testSever.js
@@ -1,2 +1,10 @@
 const http = require('http');
 
+const server = http.createServer(function(request, response) {
+  console.log(request.method);
+  console.log(request.url);
+  response.writeHead(200, {'Content-Type': 'text/html'});
+  response.write("hello");
+  response.end("end");
+
+})
